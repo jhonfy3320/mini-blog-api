b7aa6a943904afe39542443d3df77d41b6e49b99
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-28 18:51:34 -0500

    inicializar la documentación interactiva de la API Suite de Pruebas Automatizadas (Testing)

---
+++ backend/src/config/swagger.js
@@ -0,0 +1,11 @@
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./docs/openapi.yaml');
+
+module.exports = (app) => {
+  app.use(
+    '/api-docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+  );
+};
