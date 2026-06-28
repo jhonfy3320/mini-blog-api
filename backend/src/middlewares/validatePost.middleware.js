86b5ec0ff1dcb2bbbaf5e68b121d8a63e388dbec
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-28 18:36:06 -0500

    Mejorar la validación posterior

--- backend/src/middlewares/validatePost.middleware.js
+++ backend/src/middlewares/validatePost.middleware.js
@@ -1,4 +1,4 @@
-const validatePost = (req, res, next) => {
+/*const validatePost = (req, res, next) => {
   const { title, content, author_id } = req.body;
 
   if (!title || title.trim() === '') {
@@ -23,6 +23,55 @@ const validatePost = (req, res, next) => {
   }
 
   next();
+};
+
+module.exports = validatePost;*/
+const validatePost = (req, res, next) => {
+
+  const {
+    title,
+    content,
+    author_id,
+    published
+  } = req.body;
+
+  if (!title || title.trim() === '') {
+    return res.status(400).json({
+      success: false,
+      message: 'title is required'
+    });
+  }
+
+  if (!content || content.trim() === '') {
+    return res.status(400).json({
+      success: false,
+      message: 'content is required'
+    });
+  }
+
+  if (
+    author_id === undefined ||
+    author_id === null ||
+    Number.isNaN(Number(author_id))
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: 'author_id must be numeric'
+    });
+  }
+
+  if (
+    published !== undefined &&
+    typeof published !== 'boolean'
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: 'published must be boolean'
+    });
+  }
+
+  next();
+
 };
 
 module.exports = validatePost;
