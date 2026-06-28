00a8dafb21af45328e11baeb1c61b80ce56f2d6b
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-28 06:28:51 -0500

    Obtener la aplicación Express y levantar el servidor HTTP.

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
-const http = require('http');
+//const http = require('http');
 const app = require('./src/app');
+
 
 //const healthRoutes = require('./src/routes/health.routes');
 //app.use(express.json());
 //app.use('/health', healthRoutes);
-
+console.log('Starting server...');
 const PORT = process.env.PORT || 3000;
-const server = http.createServer(app);
+//const server = http.createServer(app);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on ${PORT}`);
 });
 
-server.on('error', (err) => {
+/**
+ * server.on('error', (err) => {
   console.error('SERVER ERROR:', err);
 });
+ */
