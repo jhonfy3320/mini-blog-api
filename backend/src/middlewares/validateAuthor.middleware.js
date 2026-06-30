ee9b88bcddc452b6100ed03dff541f1e7b95ac97
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-30 18:11:43 -0500

    Corrigiendo error de llamado 500

--- backend/src/middlewares/validateAuthor.middleware.js
+++ backend/src/middlewares/validateAuthor.middleware.js
@@ -1,18 +1,17 @@
 const validator = require('validator');
 
-
 const validateAuthor = (req, res, next) => {
   //console.log('AUTHOR VALIDATOR EXECUTED');
-  const { name, email } = req.body;
+  const { name = '', email = '' } = req.body || {};
 
-  if (!name || name.trim() === '') {
+  if (name.trim() === '') {
     return res.status(400).json({
       success: false,
       message: 'name is required'
     });
   }
 
-  if (!email) {
+  if (email.trim() === '') {
     return res.status(400).json({
       success: false,
       message: 'email is required'
