808d82d56834428766e82166fa51f69ab1ca58b5
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-30 21:52:25 -0500

    Cambio para el despegue

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -18,3 +18,27 @@ pool.on('error', (error) => {
 
 module.exports = pool;*/
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false
+});
+
+pool.on('connect', () => {
+  console.log('🟢 PostgreSQL connected');
+});
+
+pool.on('error', (err) => {
+  console.error('🔴 PostgreSQL Error:', err.message);
+});
+
+module.exports = pool;
