ebc51abe02f1159b3fab5002c3e700e57d81c152
jhonfyenaksmpy <ftaveraenmsafy23478@gmail.com>
2026-06-30 21:48:09 -0500

    Cambios

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,4 @@
-const { Pool } = require('pg');
+/*const { Pool } = require('pg');
 
 const pool = new Pool({
     host: process.env.DB_HOST,
@@ -16,4 +16,5 @@ pool.on('error', (error) => {
     console.error('🔴 PostgreSQL error:', error.message);
 });
 
-module.exports = pool;
+module.exports = pool;*/
+
